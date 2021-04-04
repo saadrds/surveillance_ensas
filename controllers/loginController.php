963577ba4e1c0ceb42d64f7e79442c28fa75d2fd
Hamza Habibou <info.habibou@gmail.com>
2021-04-04 23:55:35 +0100

    last loginController

--- controllers/loginController.php
+++ controllers/loginController.php
@@ -2,7 +2,7 @@
     require_once("../models/connection.php");
     $con = new ConnectionClass();
     if(isset($_POST['username']) && isset($_POST['password'])){
-        $result = $con->SelectWhereFromTable($admin,"username",$_POST['username']);
+        $result = $con->SelectWhereFromTable("admin","username",$_POST['username']);
         if(empty($result)){
             Header('Location:index.php');
         }
